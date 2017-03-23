d90208098f21f30841eed4b8cef18803a1cbab56
poonam6 <poo.poonam66@gmail.com>
2017-03-23 16:16:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res_send('article one will be here');
+  res_send('article one will be here Greetings!!!We are hiring .NET developers & senior developers who can join us in 15 to 20 days.SCHEDULE INTERVIEW DRIVE for .NET development Professionals on Saturday, 11th April 2015 between 10:00 AM to 2:00 PM. Note: Kindly send across your resume & confirm your time slot.Interested Candidates can come for Walk-in at below Venue:Faichi Solutions Pvt. Ltd.Pride Purple Accord,');
 });
 
 app.get('/ui/style.css', function (req, res) {
